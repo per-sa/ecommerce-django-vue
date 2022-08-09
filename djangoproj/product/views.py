bede7e6598b103944b38af79f3bdc65e92c69874
unpleasantpersa <ccolombomatheus@gmail.com>
2022-08-09 17:54:33 +0100

    Initial back-end endpoints and entries

--- djangoproj/product/views.py
+++ djangoproj/product/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import Http404
 # Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+class LatestProductList(APIView):
+    def get(self, request):
+        products = Product.objects.all()[:4]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
